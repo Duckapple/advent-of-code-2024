4533a8db81d7c64538c8d9d26a87e890229c4af8
Simon Green Kristensen <git@simon-green.dev>
2024-12-12 21:38:04 +0100

    feat: day 12

---
+++ 12/sol.ts
@@ -0,0 +1,84 @@
+import { adj } from "../common/array.ts";
+import { Vec2 } from "../common/array.ts";
+
+const text = await Deno.readTextFile("./12/input");
+// const text = await Deno.readTextFile("./12/example");
+// const text = await Deno.readTextFile("./12/example2");
+// const text = await Deno.readTextFile("./12/example3");
+const lines = text.split("\n");
+
+function hash(pos: Vec2) {
+  return pos[0] * lines[0].length + pos[1];
+}
+function unhash(pos: number) {
+  return [Math.floor(pos / lines[0].length), pos % lines[0].length];
+}
+
+function BFS(origin: Vec2) {
+  const visited = new Set<number>([hash(origin)]);
+  const region = { area: 0, perimeter: 0, perimCount: 0 };
+  const queue: Vec2[] = [origin];
+  const type = lines[origin[0]][origin[1]];
+  const edges: Vec2[][] = [[], [], [], []];
+
+  while (queue.length) {
+    const pos = queue.shift()!;
+    region.area++;
+    const allAdj = adj(pos);
+    for (let i = 0; i < allAdj.length; i++) {
+      const adjPos = allAdj[i];
+      const under = lines[adjPos[0]]?.[adjPos[1]];
+      if (under !== type) {
+        region.perimeter++;
+        edges[i].push(pos);
+        continue;
+      }
+      const h = hash(adjPos);
+      if (!visited.has(h)) {
+        queue.push(adjPos);
+        visited.add(h);
+      }
+    }
+  }
+
+  for (let i = 0; i < edges.length; i++) {
+    const ed = edges[i];
+    ed.sort((a, b) => a[i % 2] - b[i % 2] || a[(i + 1) % 2] - b[(i + 1) % 2]);
+    let prev = [-2, -2];
+    for (const edgeSegment of ed) {
+      const dist =
+        Math.abs(edgeSegment[0] - prev[0]) + Math.abs(edgeSegment[1] - prev[1]);
+      if (dist > 1) {
+        region.perimCount++;
+      }
+      prev = edgeSegment;
+    }
+  }
+
+  return { visited, region, type };
+}
+
+let visited = new Set<number>();
+const regions: { area: number; perimeter: number; perimCount: number }[] = [];
+
+for (let i = 0; i < lines.length; i++) {
+  const line = lines[i];
+  for (let j = 0; j < line.length; j++) {
+    if (visited.has(hash([i, j]))) continue;
+    const res = BFS([i, j]);
+    regions.push(res.region);
+    visited = visited.union(res.visited);
+  }
+}
+
+console.log(
+  "Solution 1:",
+  regions.reduce((acc, curr) => acc + curr.area * curr.perimeter, 0)
+);
+
+let sol2 = 0;
+
+console.log(
+  "Solution 2:",
+  regions.reduce((acc, curr) => acc + curr.area * curr.perimCount, 0)
+);
--- common/array.ts
+++ common/array.ts
@@ -1,3 +1,5 @@
+export type Vec2 = [number, number];
+
 // from https://youmightnotneed.com/lodash/#zip
 export const zip = <T1 extends unknown, Tx extends unknown[][]>(
   arr: T1[],
@@ -13,19 +15,25 @@ export function sub<Arr extends number[]>(arr1: Arr, arr2: Arr) {
 }
 
 // export function get<T>(arr: T[][], input: number[]): T;
-export function get<T>(arr: T[][], input: [number, number]): T;
+export function get<T>(arr: T[][], input: Vec2): T;
 export function get<T>(arr: T[][], [a, b, ...rest]: number[]) {
   return arr[a][b];
 }
 
-export function within(
-  arr: [number, number],
-  bounds: [[number, number], [number, number]]
-) {
+export function within(arr: Vec2, bounds: [Vec2, Vec2]) {
   return (
     bounds[0][0] <= arr[0] &&
     arr[0] < bounds[0][1] &&
     bounds[1][0] <= arr[1] &&
     arr[1] < bounds[1][1]
   );
+}
+
+export function adj(pos: Vec2): Vec2[] {
+  return [
+    add(pos, [-1, 0]),
+    add(pos, [0, -1]),
+    add(pos, [1, 0]),
+    add(pos, [0, 1]),
+  ];
 }
